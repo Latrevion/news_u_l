f063e87be745e8fd44d5c603f3b710253d976313
luke <xiaopikaqiu@outlook.com>
2023-11-25 22:14:41 +0800

    change theme mode

--- lib/View/screens/home_screen.dart
+++ lib/View/screens/home_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:news_u_l/Controller/news_controller.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final controller = Get.put(NewsController());
 
   @override
   Widget build(BuildContext context) {
@@ -13,18 +16,35 @@ class HomeScreen extends StatelessWidget {
               title: const Text('Closing the app'),
               content: const Text('Are you sure close the app '),
               actions: [
-                ElevatedButton(onPressed: (){
-                  Get.back(result: true); //Exit the entire application
-                }, child: const Text('close')),
-                ElevatedButton(onPressed: (){
-                  Get.back();
-                }, child: const Text('cancel')),
+                ElevatedButton(
+                    onPressed: () {
+                      Get.back(result: true); //Exit the entire application
+                    },
+                    child: const Text('close')),
+                ElevatedButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: const Text('cancel')),
               ],
             ),
           );
         },
         child: Scaffold(
-          appBar: AppBar(),
+          appBar: AppBar(
+            title: Text(
+              'News App',
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+            centerTitle: true,
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    controller.changeThemeMode();
+                  },
+                  icon: Icon(controller.iconTheme()))
+            ],
+          ),
         ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
 import 'package:news_u_l/View/screens/home_screen.dart';
 import 'package:get/get.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final box = GetStorage();
 
   // This widget is the root of your application.
   @override
@@ -15,20 +20,16 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
+      theme: ThemeData.light(
         useMaterial3: true,
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      darkTheme: ThemeData.dark(
+        useMaterial3: true,
+      ),
+      themeMode: box.read('theme') == null || box.read('theme') != 'dark'
+          ? ThemeMode.light
+          : ThemeMode.dark,
+      home: HomeScreen(),
     );
   }
 }
